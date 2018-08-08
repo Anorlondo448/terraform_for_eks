be72b7479e7b434eaea88ea2c0ae2b53be7fc7bc
Anorlondo448 <kanaend0605@gmail.com>
2018-08-09 06:19:44 +0900

    add security group rule

--- security_group.tf
+++ security_group.tf
@@ -4,8 +4,8 @@
 #
 # This security group controls networking access to the Kubernetes masters. 
 # We will later configure this with an ingress rule to allow traffic from the worker nodes.
-resource "aws_security_group" "eks" {
-  name        = "eks"
+resource "aws_security_group" "cluster" {
+  name        = "cluster"
   description = "Cluster communication with worker nodes"
   vpc_id      = "${aws_vpc.eks.id}"
 
@@ -17,6 +17,19 @@ resource "aws_security_group" "eks" {
   }
 
   tags {
-    Name = "eks"
+    Name = "cluster"
   }
+}
+
+# OPTIONAL: Allow inbound traffic from your local workstation external IP
+#           to the Kubernetes. You will need to replace A.B.C.D below with
+#           your real IP. Services like icanhazip.com can help you find this.
+resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow workstation to communicate with the cluster API Server"
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.cluster.id}"
+  to_port           = 443
+  type              = "ingress"
 }
