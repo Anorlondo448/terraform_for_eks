971cd3fff24f7558be9918e764a3b9b6401a0b55
Anorlondo448 <kanaend0605@gmail.com>
2018-08-09 06:07:26 +0900

    add iam_role

---
+++ iam_role.tf
@@ -0,0 +1,32 @@
+###
+#
+# IAM Role
+#
+resource "aws_iam_role" "eks" {
+    name = "eks"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.eks.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "eks-service-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = "${aws_iam_role.eks.name}"
+}
