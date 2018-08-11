a30e0ec314d36088ba70c2a5a084e7327304b0b5
Anorlondo448 <kanaend0605@gmail.com>
2018-08-11 12:41:09 +0900

    add sercurity group for node access to cluster

--- security_group_cluster.tf
+++ security_group_cluster.tf
@@ -33,3 +33,14 @@ resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
   to_port           = 443
   type              = "ingress"
 }
+
+# Worker Node Access to EKS Master Cluster
+resource "aws_security_group_rule" "cluster-ingress-node-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.eks-cluster.id}"
+  source_security_group_id = "${aws_security_group.eks-demo.id}"
+  to_port                  = 443
+  type                     = "ingress"
+}
