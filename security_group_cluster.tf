95d2b430e4bb1d1c14185304c7421a24b91519c9
Anorlondo448 <kanaend0605@gmail.com>
2018-08-11 15:48:52 +0900

    fix source security group

--- security_group_cluster.tf
+++ security_group_cluster.tf
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "cluster-ingress-node-https" {
   from_port                = 443
   protocol                 = "tcp"
   security_group_id        = "${aws_security_group.eks-cluster.id}"
-  source_security_group_id = "${aws_security_group.eks-demo.id}"
+  source_security_group_id = "${aws_security_group.eks-node.id}"
   to_port                  = 443
   type                     = "ingress"
 }
