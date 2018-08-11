9397726f084ff54586fe9a5b16c79155a84002e4
Anorlondo448 <kanaend0605@gmail.com>
2018-08-11 12:19:03 +0900

    rename security group to eks-cluster

--- cluster.tf
+++ cluster.tf
@@ -7,7 +7,7 @@ resource "aws_eks_cluster" "cluster" {
     role_arn = "${aws_iam_role.eks-cluster.arn}"
 
     vpc_config {
-        security_group_ids =["$aws_security_group.eks.id"]
+        security_group_ids =["$aws_security_group.eks-cluster.id"]
         subnet_ids = [
             "${aws_subnet.public-1a.id}",
             "${aws_subnet.public-1c.id}"
--- security_group_cluster.tf
+++ security_group_cluster.tf
@@ -1,11 +1,11 @@
 ###
 #
-# Security Group
+# Security Group for Cluster
 #
 # This security group controls networking access to the Kubernetes masters. 
 # We will later configure this with an ingress rule to allow traffic from the worker nodes.
-resource "aws_security_group" "cluster" {
-  name        = "cluster"
+resource "aws_security_group" "eks-cluster" {
+  name        = "eks-cluster"
   description = "Cluster communication with worker nodes"
   vpc_id      = "${aws_vpc.eks.id}"
 
@@ -29,7 +29,7 @@ resource "aws_security_group_rule" "cluster-ingress-workstation-https" {
   description       = "Allow workstation to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
-  security_group_id = "${aws_security_group.cluster.id}"
+  security_group_id = "${aws_security_group.eks-cluster.id}"
   to_port           = 443
   type              = "ingress"
 }
