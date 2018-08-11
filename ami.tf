13f292af145970125387c8c401a15a12c01f28d6
Anorlondo448 <kanaend0605@gmail.com>
2018-08-11 16:17:28 +0900

    exexute terraform fmt

--- ami.tf
+++ ami.tf
@@ -4,10 +4,10 @@
 #
 data "aws_ami" "eks-worker" {
   filter {
-    name = "name"
+    name   = "name"
     values = ["eks-worker-*"]
   }
 
   most_recent = true
-  owners = ["602401143452"] # eks-worker AMI Owner
+  owners      = ["602401143452"] # eks-worker AMI Owner
 }
